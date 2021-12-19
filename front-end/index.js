9e6a83aeda32a81ada40f5af34138337bebb3501
Saúl Blanco <saul101.pi@outlook.com>
2021-12-19 16:36:13 +0100

    Implemented basic express front end web server

---
+++ front-end/index.js
@@ -0,0 +1,17 @@
+import express from "express"
+import queue from "./queue.js"
+
+const app = express();
+const PORT = process.env.PORT | 8080;
+
+app.use(express.json());
+
+app.post("/", async (req, res) => {
+    const message = req.body.message;
+    const result = queue.publish(message);
+    res.send(result);
+});
+
+app.listen(PORT, () => {
+    console.log(`Front end listening on port ${PORT}`);
+})
