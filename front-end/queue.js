e7bb72590d9ca809c183684cd57a1241aae5ad3c
mmatagne <miromanuel.matagne@gmail.com>
2021-12-26 22:35:46 +0100

    added worker

--- front-end/queue.js
+++ front-end/queue.js
@@ -1,25 +1,26 @@
-import {connect, Empty, StringCodec} from "nats"
+import { connect, Empty, StringCodec } from "nats";
 
 const QUEUE_ADDR = process.env.QUEUE_ADDR;
 const TOPIC = "job";
 
 async function connect() {
-    return await connect({servers: QUEUE_ADDR});
+  return await connect({ servers: QUEUE_ADDR });
 }
 const queue = await connect();
 
 const sc = StringCodec();
 
 const publish = (msg) => {
-    return await queue.request(TOPIC, sc.encode(msg), {timeout: 2000})
-        .then(m => {
-            return sc.decode(m.data);
-        })
-        .catch(() => {
-            return null;
-        });
+  return await queue
+    .request(TOPIC, sc.encode(msg), { timeout: 2000 })
+    .then((m) => {
+      return sc.decode(m.data);
+    })
+    .catch(() => {
+      return null;
+    });
 };
 
 export default {
-    publish,
+  publish,
 };
