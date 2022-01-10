a8ea0c423ee72ae3e80bb2cdc74b77150c40c836
mmatagne <miromanuel.matagne@gmail.com>
2022-01-10 16:40:54 +0100

    hardcoded QUEUE_ADDR

--- worker/index.js
+++ worker/index.js
@@ -1,6 +1,7 @@
 import { connect, StringCodec } from "nats";
 
-const QUEUE_ADDR = process.env.QUEUE_ADDR || "localhost";
+// const QUEUE_ADDR = process.env.QUEUE_ADDR || "localhost";
+const QUEUE_ADDR = "0.queue";
 const QUEUE_PORT = 4222;
 const TOPIC = "job";
 
@@ -8,7 +9,9 @@ async function asyncConnect() {
   try {
     const queue = await connect({ servers: `${QUEUE_ADDR}:${QUEUE_PORT}` });
     console.log("Connected to queue");
-    process.on("exit", () => { queue.close(); });
+    process.on("exit", () => {
+      queue.close();
+    });
     return queue;
   } catch (err) {
     console.log(`Error connecting to ${QUEUE_ADDR}`);
@@ -19,8 +22,7 @@ async function asyncConnect() {
 const queue = await asyncConnect();
 const sc = StringCodec();
 
-if (queue == null)
-  process.exit(1);
+if (queue == null) process.exit(1);
 
 const sub = queue.subscribe(TOPIC);
 (async (inSub) => {
